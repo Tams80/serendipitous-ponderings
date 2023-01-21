dbde8bf8ae3fd1d69f8a576800f041cd94a35e09
Tams80 <121610095+Tams80@users.noreply.github.com>
2023-01-21 20:15:52 +0900

    Fixed links with CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-const tabs = document.querySelectorAll(".tab");
-const menus = document.querySelectorAll(".menu div");
+function openSection(evt, sectionName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(sectionName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
-// Add click event to tabs
-tabs.forEach(tab => {
-    tab.addEventListener("click", function() {
-        // Remove active class from all tabs
-        tabs.forEach(tab => tab.classList.remove("active"));
-        // Add active class to current tab
-        this.classList.add("active");
-        // Hide all menus
-        menus.forEach(menu => menu.style.display = "none");
-        // Show the corresponding menu
-        const id = this.getAttribute("id").split("tab")[1];
-        const menu = document.querySelector(`#menu${id} div`);
-        menu.style.display = "block";
-    });
+document.addEventListener("mouseout", function(event) {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    var tablinks = document.getElementsByClassName("tablinks");
+
+    for (var i = 0; i < tabcontent.length; i++) {
+        if (event.relatedTarget == null || !tabcontent[i].contains(event.relatedTarget)) {
+            tabcontent[i].style.display = "none";
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+    }
 });
 
-// Add click event to menu items
-menus.forEach(menu => {
-    menu.addEventListener("click", function() {
-        // Remove active class from all menu items
-        menus.forEach(menu => menu.classList.remove("active"));
-        // Add active class to current menu item
-        this.classList.add("active");
+// Add event listeners to the tab buttons to open the tab on click or hover
+var tablinks = document.getElementsByClassName("tablinks");
+for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].addEventListener("mouseover", function(event) {
+        openSection(event, event.target.innerHTML);
     });
-});
+    tablinks[i].addEventListener("click", function(event) {
+        openSection(event, event.target.innerHTML);
+    });
+}
